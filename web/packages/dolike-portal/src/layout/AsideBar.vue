63c8c39ed7b96a44b5b271fc293fd20fdb6069c6
lovesakura <developer@weibo-app.local>
2026-05-28 21:48:01 +0800

    feat: 帮助按钮链接到 GitHub README

--- web/packages/dolike-portal/src/layout/AsideBar.vue
+++ web/packages/dolike-portal/src/layout/AsideBar.vue
@@ -95,7 +95,13 @@ const goToMyPage = () => {
                 <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z"/>
               </svg>
             </a>
-            <div class="footer-action" title="帮助">
+            <a
+              class="footer-action footer-action--link"
+              title="帮助"
+              href="https://github.com/lovesakuratears/DoLike/blob/main/README.md"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <div class="footer-action__icon footer-action__icon--help">
                 <svg
                   viewBox="0 0 24 24"
@@ -104,7 +110,6 @@ const goToMyPage = () => {
                   width="1em"
                   height="1em"
                   focusable="false"
-                  id="douyin-sidebar-new"
                 >
                   <path
                     fill-rule="evenodd"
@@ -114,7 +119,7 @@ const goToMyPage = () => {
                   ></path>
                 </svg>
               </div>
-            </div>
+            </a>
         </div>
       </div>
     </div>
